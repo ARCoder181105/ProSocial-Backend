8c76c861f8d57b7341dd22622dce17787acf4061
Aditya Rana <adityarana181105@gmail.com>
2025-10-16 16:08:22 +0530

    Enhance post schema with additional fields
    Added tags, image, likes, comments, isPublished, and views fields to the post schema.

--- models/post.models.js
+++ models/post.models.js
@@ -4,16 +4,55 @@ const postSchema = new mongoose.Schema(
   {
     author: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User", 
-      required: true
+      ref: "User",
+      required: true,
     },
     title: {
       type: String,
-      required: [true, 'Please enter title']
+      required: [true, "Please enter title"],
     },
     content: {
       type: String,
-      required: [true, 'Please provide some content']
+      required: [true, "Please provide some content"],
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    image: {
+      type: String, // URL of post image
+      default: null,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // who liked the post
+      },
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
     },
   },
   { timestamps: true }
